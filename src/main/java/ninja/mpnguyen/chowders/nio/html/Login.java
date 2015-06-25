788a47b92f6962456a83119a8f20fa09db23abf2
Mark Nguyen <mpnguyen@umail.ucsb.edu>
2015-06-25 00:24:26 -0700

    Refactored Login to be streamlined with three distinct phases and implements Callable

--- src/main/java/ninja/mpnguyen/chowders/nio/html/Login.java
+++ src/main/java/ninja/mpnguyen/chowders/nio/html/Login.java
@@ -11,14 +11,14 @@ import org.jsoup.nodes.Document;
 
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.concurrent.Callable;
 
 import ninja.mpnguyen.chowders.nio.RawGet;
 import ninja.mpnguyen.chowders.nio.Post;
 import ninja.mpnguyen.chowders.things.html.Auth;
 
-public class Login extends Post<Auth> {
+public class Login implements Callable<Auth> {
     private final String login, password;
-    private Params params;
 
     public Login(String login, String password) {
         this.login = login;
@@ -26,73 +26,52 @@ public class Login extends Post<Auth> {
     }
 
     @Override
-    public Auth post() throws IOException {
-        params = new ParameterFetcher().get();
-        return super.post();
-    }
+    public Auth call() throws IOException {
+        // Phase 1 is to initialize with the default cookie and authentication token
+        OkHttpClient client = new OkHttpClient();
+        client.setFollowRedirects(false);
+        client.setFollowSslRedirects(false);
 
-    @Override
-    public RequestBody getBody() {
-        return new FormEncodingBuilder()
+        Request initRequest = new Request.Builder()
+                .url("https://lobste.rs/login")
+                .build();
+
+        Response initResponse = client.newCall(initRequest).execute();
+        String initCookie = initResponse.header("Set-Cookie").split(";")[0];
+        String initBody = initResponse.body().string();
+        String authenticity_token = Jsoup.parse(initBody).body().select("[name=authenticity_token]").val();
+
+        // Phase 2 is to authenticate given the cookie and authentication token
+        RequestBody loginBody = new FormEncodingBuilder()
                 .add("utf8", "\u2713")
-                .add("authenticity_token", params.authenticity_token)
-                .add("email", "markerz")
+                .add("authenticity_token", authenticity_token)
+                .add("email", login)
                 .add("password", password)
                 .add("commit", "Login")
                 .add("referer", "https://lobste.rs/")
                 .build();
-    }
-
-    @Override
-    public String getEndpoint() {
-        return "login";
-    }
-
-    @Override
-    public Auth handle(Response response) throws IOException {
-        String cookie = response.header("Set-Cookie").split(";")[0];
-
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder()
-                .url("https://lobste.rs/settings")
-                .header("Cookie", cookie)
+        Request loginRequest = new Request.Builder()
+                .url("https://lobste.rs/login")
+                .header("Cookie", initCookie) // We must use #header instead of #addHeader
+                .post(loginBody)
                 .build();
-        Response r = client.newCall(request).execute();
-        String html = r.body().string();
-        Document dom = Jsoup.parse(html);
+
+        Response loginResponse = client.newCall(loginRequest).execute();
+        String loginCookie = loginResponse.header("Set-Cookie").split(";")[0];
+
+        // Phase 3 is to grab the actual username/email from the settings page
+        Request detailsRequest = new Request.Builder()
+                .url("https://lobste.rs/settings")
+                .header("Cookie", loginCookie)
+                .build();
+        Response detailsResponse = client.newCall(detailsRequest).execute();
+        String detailsHtml = detailsResponse.body().string();
+
+        Document dom = Jsoup.parse(detailsHtml);
         String username = dom.select("#user_username").val();
         String email = dom.select("#user_email").val();
 
-        return new Auth(email, username, cookie);
-    }
-
-    @Override
-    public Auth getAuthentication() {
-        return new Auth("", "", params.cookie);
-    }
-
-    private static class Params implements Serializable {
-        public final String authenticity_token, cookie;
-
-        private Params(String authenticity_token, String cookie) {
-            this.authenticity_token = authenticity_token;
-            this.cookie = cookie;
-        }
-    }
-
-    private static class ParameterFetcher extends RawGet<Params> {
-        @Override
-        public Params handle(Response response) throws IOException {
-            String cookie = response.header("Set-Cookie").split(";")[0];
-            String body = response.body().string();
-            String authenticity_token = Jsoup.parse(body).body().select("[name=authenticity_token]").val();
-            return new Params(authenticity_token, cookie);
-        }
-
-        @Override
-        public String getEndpoint() {
-            return "login";
-        }
-
+        // And then we return the result of all three phases
+        return new Auth(email, username, loginCookie);
     }
 }
